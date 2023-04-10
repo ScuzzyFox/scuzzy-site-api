909fe6517ac19d0be7030d1d5f029ca7c2ac5f7d
scuzzyfox <scuzzyfox@gmail.com>
2023-04-10 15:15:33 -0600

    added documentation comments

--- siteapi/goals/serializers.py
+++ siteapi/goals/serializers.py
@@ -4,7 +4,13 @@ from django.utils.text import slugify
 
 
 class GoalSerializer(serializers.ModelSerializer):
-    """Used for Create, Update, Delete actions"""
+    """
+    Serializer class for creating, updating and deleting a Goal instance. Returns a serialized version of the model
+    that includes an `image_url` property with an absolute URL to the image file. The `image_url` is generated using
+    the `get_image_url()` method, which uses the `request` object to build an absolute URI to the image file.
+    """
+
+    # image_url is a property that is tied to the return of a mehtod. in this case the default is get_image_url
     image_url = serializers.SerializerMethodField()
 
     class Meta:
@@ -16,19 +22,57 @@ class GoalSerializer(serializers.ModelSerializer):
             "date_fulfilled": {"required": False},
             "use_case": {"required": False},
             "slug": {"required": False},
+
+            # prevent image property from being returned
             "image": {"write_only": True}
 
         }
 
     def get_image_url(self, object):
+        """
+     Returns the absolute URL of the goal image.
+
+     Args:
+         object (Goal): The instance of the goal being serialized.
+
+     Returns:
+         str: The absolute URL of the image associated with the goal in https.
+
+     """
         request = self.context.get('request')
         object_url = object.image.url
-        return request.build_absolute_uri(object_url)
+
+        # Replace http with https in the image URL.
+        # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+        # &
+        # USE_X_FORWARDED_HOST = True
+        # in settings would get around this, but I don't mind doing this for now.
+        # this is a result of using nginx as a reverse proxy.
+        return request.build_absolute_uri(object_url).replace('http://', 'https://')
 
     def create(self, validated_data):
+        """
+        Creates a new instance of the Goal model with the validated data and sets its slug based on its name.
+
+        Args:
+            validated_data (dict): The validated data to create the instance.
+
+        Returns:
+            Goal: The newly created instance.
+        """
         validated_data["slug"] = slugify(validated_data["name"].lower())
         return super().create(validated_data=validated_data)
 
     def update(self, instance, validated_data):
+        """
+        Updates an existing instance of the Goal model with the validated data and updates its slug based on its name.
+
+        Args:
+            instance (Goal): The instance to update.
+            validated_data (dict): The validated data to update the instance.
+
+        Returns:
+            Goal: The updated instance.
+        """
         validated_data["slug"] = slugify(validated_data["name"].lower())
         return super().update(instance, validated_data=validated_data)
--- siteapi/goals/views.py
+++ siteapi/goals/views.py
@@ -7,54 +7,118 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
 
-# Create your views here.
-
-
 class ListGoals(generics.ListAPIView):
+    """
+    A view that returns a list of goals.
+
+    This view has no authentication or permission requirements.
+    """
     permission_classes = ()
     serializer_class = GoalSerializer
     queryset = Goal.objects.all()
 
 
 class GoalDetail(generics.RetrieveAPIView):
+    """
+    A view that returns a single goal by ID.
+
+    This view has no authentication or permission requirements.
+    """
     permission_classes = ()
     serializer_class = GoalSerializer
     queryset = Goal.objects.all()
 
 
 class GoalDetailSlug(generics.RetrieveAPIView):
+    """
+    A view that returns a single goal by slug.
+
+    This view has no authentication or permission requirements.
+    """
     permission_classes = ()
     serializer_class = GoalSerializer
     queryset = Goal.objects.all()
     lookup_field = "slug"
 
 
-# update/delete a specific goal
 class UDGoals(generics.GenericAPIView, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
+    """
+    A view that handles updating and deleting a specific goal by ID.
+
+    This view requires JWT authentication.
+    """
     authentication_classes = [JWTAuthentication]
     serializer_class = GoalSerializer
     queryset = Goal.objects.all()
 
     def put(self, request, *args, **kwargs):
+        """
+        Update a specific goal by ID.
+
+        This endpoint requires a JWT token in the request header.
+
+        :param request: The HTTP request object.
+        :param args: Additional arguments passed to the view.
+        :param kwargs: Additional keyword arguments passed to the view.
+        :return: An HTTP response with the updated goal data.
+        """
         return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
+        """
+        Delete a specific goal by ID.
+
+        This endpoint requires a JWT token in the request header.
+
+        :param request: The HTTP request object.
+        :param args: Additional arguments passed to the view.
+        :param kwargs: Additional keyword arguments passed to the view.
+        :return: An HTTP response indicating whether the goal was successfully deleted.
+        """
         return self.destroy(request, *args, **kwargs)
 
 
 class CreateGoal(generics.GenericAPIView, mixins.CreateModelMixin):
+    """
+    A view that handles creating a new goal.
 
+    This view requires JWT authentication.
+    """
     authentication_classes = [JWTAuthentication]
     serializer_class = GoalSerializer
 
     def post(self, request, *args, **kwargs):
+        """
+        Create a new goal.
+
+        This endpoint requires a JWT token in the request header.
+
+        :param request: The HTTP request object.
+        :param args: Additional arguments passed to the view.
+        :param kwargs: Additional keyword arguments passed to the view.
+        :return: An HTTP response with the newly created goal data.
+        """
         return self.create(request, *args, **kwargs)
 
 
 class FulfillGoal(APIView):
+    """
+    A view that handles marking a goal as fulfilled.
+
+    This view requires JWT authentication.
+    """
     authentication_classes = [JWTAuthentication]
 
     def put(self, request, pk):
+        """
+        Mark a goal as fulfilled.
+
+        This endpoint requires a JWT token in the request header.
+
+        :param request: The HTTP request object.
+        :param pk: The primary key of the goal to be fulfilled.
+        :return: An HTTP response with the updated goal data.
+        """
         goal = get_object_or_404(Goal, pk=pk)
         goal.make_fulfilled()
         serializer = GoalSerializer(goal, context={"request": request})
