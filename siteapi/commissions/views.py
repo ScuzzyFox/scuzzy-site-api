5091b3ec481a25d796a83177ab0e8f76fa9dbd52
scuzzyfox <scuzzyfox@gmail.com>
2023-06-17 18:12:35 -0600

    debugging addremoveoption

--- siteapi/commissions/views.py
+++ siteapi/commissions/views.py
@@ -102,15 +102,25 @@ class CommissionAddRemoveOption(APIView):
 
     def post(self, request, commission_id, option_id, *args, **kwargs):
         commission = get_object_or_404(Commission, id=commission_id)
+        initial_count_of_options = len(commission.options)
         option = get_object_or_404(CommissionOption, id=option_id)
         commission.options.add(option)
-        return Response(CommissionSerializer(commission, context={'request': request}).data)
+        if initial_count_of_options < len(commission.options):
+            return Response(CommissionSerializer(commission, context={'request': request}).data)
+        else:
+            return Response({'error': "commission options did not increase. Failed to add option "+option_id+" to commission "+commission_id})
 
     def delete(self, request, commission_id, option_id, *args, **kwargs):
         commission = get_object_or_404(Commission, id=commission_id)
+        initial_count_of_options = len(commission.options)
+
         option = get_object_or_404(CommissionOption, id=option_id)
         commission.options.remove(option)
-        return Response(CommissionSerializer(commission, context={'request': request}).data)
+        if initial_count_of_options < len(commission.options):
+
+            return Response(CommissionSerializer(commission, context={'request': request}).data)
+        else:
+            return Response({'error': "commission options did not decrease. Failed to remove option "+option_id+" from commission "+commission_id})
 
 
 # get all commission options or make a new one
